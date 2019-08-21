cf84284bbfe1a15712daf4560026cd6dcc65ac89
turinglabsorg <turinglabs@tuta.io>
2019-08-21 10:31:59 +0200

    secure script fix

--- secure.sh
+++ secure.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
-#CREATING CERTIFICATE
-if [$1]
-    DOMAIN = $1
-    cd /etc/nginx/sites-enabled
-    touch "${DOMAIN}.conf"
+#CHECK FOR DOMAIN
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+  else
+    #DELETE DEFAULT CONFIGURATION
+    rm /etc/nginx/sites-enabled/default
+
+    #CREATE NEW CONFIGURATION
+    touch "/etc/nginx/sites-enabled/default"
     echo "server {
-            server_name ${DOMAIN};
-            # The internal IP of the VM that hosts your Apache config
-            set $upstream 127.0.0.1:1337;
+            server_name $1;
             location / {
-                proxy_pass_header Authorization;
-                proxy_pass http://$upstream;
-                proxy_set_header Host $host;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                proxy_http_version 1.1;
-                proxy_set_header Connection “”;
-                proxy_buffering off;
-                client_max_body_size 0;
-                proxy_read_timeout 36000s;
-                proxy_redirect off;
+                proxy_pass http://127.0.0.1:1337;
             }
             listen 80;
-        }" > "${DOMAIN}.conf"
-        
-        systemctl reload nginx
-        certbot — nginx -d ${DOMAIN}
-else
-    echo "SPECIFY DOMAIN"
+        }" > "/etc/nginx/sites-enabled/default"
+
+    #RELOAD NGINX
+    systemctl reload nginx
+    #PRINT SUCCESS MESSAGE
+    echo "BASIC SETUP IS COMPLETE, PLEASE RUN: sudo certbot --nginx -d $1"
 fi
